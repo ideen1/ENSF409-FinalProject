4c41577b07a38462d5f299623f814aa70314569d
Ideen Banijamali <ideen@Ideens-MBP.unifi.ideen.ca>
2022-03-24 18:41:43 -0600

    Default Class File structures

---
+++ edu/ucalgary/ensf409/Default.java
@@ -0,0 +1,27 @@
+/** *Default*.java 
+ *  Java Class file for ENSF409 Final Project 202 - 
+ *  Winter 2022 - Group 5
+ *  Copyright © 2022 I.B., T.D., M.G.
+ *  @author *INSERT NAME*
+ *  @version 1.0
+ *  @since *INSERT SINCE VERSION*
+ */
+
+
+
+package edu.ucalgary.ensf409;
+
+public class Default {
+    
+    /**
+     * *methodName*
+     * @param 1,2,3
+     * @return
+     */
+
+    public void methodName() {
+        
+    }
+}
+
+// Copy this class file when creating new files to include proper documentation, replace words between *...*
