508e2b2796d7bd5fb74bbaeed7bff7a162044041
maostar1010 <79169577+maostar1010@users.noreply.github.com>
2022-04-01 13:19:04 -0600

    testing titles and descriptions updated

--- edu/ucalgary/ensf409/InventoryService.java
+++ edu/ucalgary/ensf409/InventoryService.java
@@ -7,29 +7,35 @@ import java.util.HashMap;
 public class InventoryService{
 	private Hamper[] hampersToCheck;
 	private static Inventory inventory;
-	private HashMap<String, Boolean> missingCategory;
+	private HashMap<String, Boolean> missingCategory = new HashMap<String, Boolean>();
 	
 	public void inventoryCheckAlgorithm() {
-	
+		
+		/*
+		 * fillHamper();
+		 * if missingCategory is empty
+		 * 		for all food items stored in each Hamper's allocatedItems // OR // all the FoodItem whose used field is true
+		 * 			Inventory.deleteFoodItem(usedItem)
+		 * 		create an order form (.txt)
+		 * else 
+		 * 		reset the tmpUsed field of all FoodItem's in the Inventory to "false"
+	 * 			throw the custom exception InventoryNotAvailableException, with a message indicating which food category 
+	 * 			is short (missingCategory content)
+		 */
 	}
 	
 	// Helper methods
-	private void fillHamper() {
+	private void fillHampers() {
 		// used in a loop, helper method for inventoryCheckAlgorithm
 		// selects food items from the inventory and updates the tmpUsed in foodItems
 		// updates missingCategory if missing anything for a hamper
 	}
-	private boolean checkHamperAvail() {
-		// checks each Hamper's canBeFulfilled part
-		
-		return true; //temporary filler
-	}
-	private void deleteTmpUsedItems() {
-		// updates inventory depending on whether or not all hampers can be filled
-	}
 			
 	// Constructor
-	public InventoryService(Hamper[] hampers, Inventory inventory) {}
+	public InventoryService(Hamper[] hampers) {
+		hampersToCheck = hampers;
+		inventory = new Inventory();
+	}
 	
 }
 //
