73834145ff7654e8c7cf03b8a8ab2d0e16490455
666dragonz <93061675+666dragonz@users.noreply.github.com>
2022-04-01 12:39:59 -0600

    New updates

--- edu/ucalgary/ensf409/TanishTests.java
+++ edu/ucalgary/ensf409/TanishTests.java
@@ -3,6 +3,8 @@ package edu.ucalgary.ensf409;
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.io.*;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.*;
 
 
@@ -12,10 +14,11 @@ public class TanishTests {
     public void testAddHamper() {
         int expectedlistsize = Request.getHampers().size() + 1;
 
-        Request.addHamper();
+        Request.addHamper("Test client", 1, 1, 1, 1);
         int actuallistsize = Request.getHampers().size();
         
         assertEquals("The hamper array list did not get updated",expectedlistsize, actuallistsize);
+        
     }
 
     @Test 
@@ -43,6 +46,15 @@ public class TanishTests {
         // convert txt created by genarateOrderForm method to a string and compare that to expectedOrderString
 
         assertEquals("The actual string did not match the expected string",expectedOrderString, actualOrderString);
+    }
+    @Test
+    public void TesttwoHampersWithSameClient(){
+        Request request = new Request("test", LocalDate.now());
+        request.addHamper("client 1", 1, 1, 1, 1);
+        request.addHamper("client 1", 2, 2, 2, 2);
+
+        assertFalse("Second hamper with the same client name is a copy of the first hamper", request.getHampers().get(0) ==request.getHampers().get(1));
+        assertTrue("The second client request didn't get made", request.getHampers().get(1) != null);
     }
 
     //testing iventory class 
