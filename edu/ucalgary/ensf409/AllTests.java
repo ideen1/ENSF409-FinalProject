99e997c106339c5dd75e7429382bebd9e620f466
666dragonz <93061675+666dragonz@users.noreply.github.com>
2022-04-01 23:42:29 -0600

    Update AllTests.java

--- edu/ucalgary/ensf409/AllTests.java
+++ edu/ucalgary/ensf409/AllTests.java
@@ -2,7 +2,7 @@
  *  Java Class file for ENSF409 Final Project 202 - 
  *  Winter 2022 - Group 5
  *  Copyright © 2022 I.B., T.D., M.M.
- *  @author Tanish
+ *  @author Ideen, Tanish, Mary
  *  @version 1.0
  *  @since 1.0
  */
@@ -10,9 +10,13 @@
 package edu.ucalgary.ensf409;
 
 import org.junit.*;
-import static org.junit.Assert.*;
 import java.time.LocalDate;
 import java.util.*;
+import static org.junit.Assert.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.HashMap;
 
 
 public class TanishTests {
