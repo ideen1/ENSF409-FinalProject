b959c9184ffc09e9ba7b7b7bb534b7e3e2246386
maostar1010 <79169577+maostar1010@users.noreply.github.com>
2022-04-01 21:56:03 -0600

    Update TanishTests.java

--- edu/ucalgary/ensf409/TanishTests.java
+++ edu/ucalgary/ensf409/TanishTests.java
@@ -1,7 +1,7 @@
 /** TanishTests.java 
  *  Java Class file for ENSF409 Final Project 202 - 
  *  Winter 2022 - Group 5
- *  Copyright © 2022 I.B., T.D., M.G.
+ *  Copyright © 2022 I.B., T.D., M.M.
  *  @author Tanish
  *  @version 1.0
  *  @since 1.0
