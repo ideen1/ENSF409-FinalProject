272009fa2845f8555809f2c8a4b4ab75adbfe71a
maostar1010 <79169577+maostar1010@users.noreply.github.com>
2022-04-01 23:19:00 -0600

    Update TestingMary.java

--- edu/ucalgary/ensf409/TestingMary.java
+++ edu/ucalgary/ensf409/TestingMary.java
@@ -9,12 +9,45 @@ import java.util.HashMap;
 import org.junit.*;
 
 public class TestingMary {
+	private Hamper hamper1 = new Hamper("client1",1,0,0,0);
+	private Hamper hamper2 = new Hamper("client2", 0,0,0,1);
+
+	hamper1.getPeople().get(0).getNutrition().setTotalNeedCalories(3000);
+
+    hamper2.getPeople().get(0).getNutrition().setTotalNeedCalories(1000);
 	
+	private Hamper hamper3 = new Hamper("client3-cannotBeFilled", 2,2,2,2);
+	private Hamper hamper4 = new Hamper("client4-canBeFilled", 0,0,1,0);
+
+	hamper3.getPeople().get(0).getNutrition().setTotalNeedCalories(3000);
+	hamper3.getPeople().get(1).getNutrition().setTotalNeedCalories(3000);
+    hamper3.getPeople().get(2).getNutrition().setTotalNeedCalories(2500);
+	hamper3.getPeople().get(3).getNutrition().setTotalNeedCalories(2500);
+    hamper3.getPeople().get(4).getNutrition().setTotalNeedCalories(1500);
+	hamper3.getPeople().get(5).getNutrition().setTotalNeedCalories(1500);
+	hamper3.getPeople().get(6).getNutrition().setTotalNeedCalories(1000);
+	hamper3.getPeople().get(7).getNutrition().setTotalNeedCalories(1000);
+
+	hamper4.getPeople().get(0).getNutrition().setTotalNeedCalories(1500);
+
+	private Hamper[] hampers_good = {hamper1, hamper2};
+	private Hamper[] hampers_bad = {hamper3, hamper4};
+
+	private Inventory inventory = new Inventory();
+
+	private FoodItem testItem = new FoodItem(1, "eggs", 30, 10, 20, 45,2000);       //Creating an example test item
+    private FoodItem testItem2 = new FoodItem(2, "bread", 34, 12, 27, 42,1023);     //Creating a second example test item
+    private FoodItem testItem3 = new FoodItem(3, "milk", 20, 60, 50, 70,1000);      //Creating a third example test item
+
+    inventory.addFoodItem(testItem);                                        //Adding test item to Foodlist in Inventory
+    inventory.addFoodItem(testItem2);                                       //Adding second test item to Foodlist in Inventory
+    inventory.addFoodItem(testItem3);                                       //Adding third test item to Foodlist in Inventory
+
+	// inventory.removeFoodItem(testItem2);                                    //Removing item using FoodItem Argument
+    // inventory.removeFoodItem(1);                                            //Removing item using test item ID
+
 	/* What do I want to test?????
-	 * 
-	 * Need a Hamper[] and an Inventory objects to construct an object of InventoryService class with
-	 * 
-	 * The constructor should set the hampersToCheck and inventory properly
+	
 	 * 
 	 * fillHamper() should properly update the tmpUsed field in appropriate FoodItem obj, the canBeFulfilled 
 	 * and the allocatedItems fields in each Hamper, and the missingCategory filed if needed.
@@ -28,20 +61,96 @@ public class TestingMary {
 	 * 			reset the tmpUsed field of all FoodItem's in the Inventory to "false"
 	 * 			throw the custom exception InventoryNotAvailableException, with a message indicating which food category 
 	 * 			is short (missingCategory content)
-	 * 
-	 * 
-	 * 
 	 */
+//
+	/*
+	 * InventoryService(Hamper[]) is called with a list of hampers that can all be filled.
+	 * getHampers() returns correct ArrayList<Hampers> that is stored as hampersToCheck.
+	 * getInventory() returns the correct Inventory that is stored as inventory. 
+	 */
+	@Test
+	public void testGetters(){
+		
+		ArrayList<Hamper> expectedHampers = new ArrayList<Hamper>();
+		expectedHampers.add(hamper1);
+		expectedHampers.add(hamper2);
+
+		Inventory expectedInventory = new Inventory();
+		
+		InventoryService check = new InventoryService(hampers_good);
+		ArrayList<Hamper> actualHampers = check.getHampers();
+		Inventory actualInventory = check.getInventory();
+
+		assertEquals("The getHampers() method did not return the correct ArrayList<Hamper>", expectedHampers, actualHampers);
+		assertEquals("The getInventory() method did not return the correct Inventory", expectedInventory, actualInventory);
+	}
 	
-	private final Hamper HAMPER1 = new Hamper("client1",2,1,1,1);
-	private final Hamper HAMPER2 = new Hamper("client2", 1,2,1,1);
-	
-	private final Hamper HAMPER3 = new Hamper("client3-cannotBeFilled", 3,3,3,3);
-	private final Hamper HAMPER4 = new Hamper("client4-canBeFilled", 0,0,1,1);
-	
-	private final Hamper[] HAMPERS1 = {HAMPER1, HAMPER2};
-	private final Hamper[] HAMPERS2 = {HAMPER2, HAMPER2};
-	
-	private final INVENTORY = new Inventory();
+	/*
+	 * InventoryService(Hamper[]) is called with a list of hampers that can all be filled.
+	 * inventoryCheckAlgorithm() calls fillHampers() which then updates tmpUsed field in appropriate FoodItem objects to 
+	 * "true", the canBeFulfilled field in each Hamper to "true", and adds appropriate FoodItem objects to the 
+	 * allocatedItems field of each hamper.
+	 */
+	@Test
+	public void testFillHampersCorrectlyUpdatesHampersAndFoodItems(){
+		InventoryService check = new InventoryService(hampers_good);
+		check.inventoryCheckAlgorithm();
+
+		boolean hamperFilledReturn1 = check.getHampers().get(0).getCanBeFulfilled();
+		boolean hamperFilledReturn2 = check.getHampers().get(1).getCanBeFulfilled();
+
+		int returnItemID1 = check.getHampers().get(0).getAllocatedItem(0);
+		int returnItemID1 = check.getHampers().get(0).getAllocatedItem(1);
+		int returnItemID1 = check.getHampers().get(1).getAllocatedItem(0);
+
+		boolean returnValue1 = check.getInventory().getFoodItem(1).getTmpUsed();
+		boolean returnValue2 = check.getInventory().getFoodItem(2).getTmpUsed();
+		boolean returnValue3 = check.getInventory().getFoodItem(3).getTmpUsed();
+
+		assertTrue("The Hamper's canBeFulfilled field was not correctly updated (1)", hamperFilledReturn1);
+		assertTrue("The Hamper's canBeFulfilled field was not correctly updated (2)", hamperFilledReturn2);
+
+		assertEquals("FoodItem's were not correctly stored in the Hamper's allocatedItems field", 1, rturnItemID1);
+		assertEquals("FoodItem's were not correctly stored in the Hamper's allocatedItems field", 2, rturnItemID2);
+		assertEquals("FoodItem's were not correctly stored in the Hamper's allocatedItems field", 3, rturnItemID3);		
+
+		assertTrue("The FoodItem's tmpUsed field was not correctly updated (1)", returnValue1);
+		assertTrue("The FoodItem's tmpUsed field was not correctly updated (2)", returnValue2);
+		assertTrue("The FoodItem's tmpUsed field was not correctly updated (3)", returnValue3);
+	}
+
+	/*
+	 * InventoryService(Hamper[]) is called with a list of hampers where the first hamper cannot be filled.
+	 * inventoryCheckAlgorithm() calls fillHampers() which then updates tmpUsed field in appropriate FoodItem objects to 
+	 * "true", the canBeFulfilled field in the first Hamper to "false", adds the missing food category to the 
+	 * missingCategory array, and continues to check the second hamper in the hampersToCheck array. 
+	 */
+	@Test
+	public void testFillHampersCorrectlyUpdatesMissingCategory(){
+
+	}
+
+	/*
+	 * InventoryService(Hamper[]) is called with a list of hampers that can all be filled.
+	 * inventoryCheckAlgorithm() calls fillHampers() which then updates tmpUsed field in appropriate FoodItem objects to 
+	 * "true", the canBeFulfilled field in each Hamper to "true", and adds appropriate FoodItem objects to the 
+	 * allocatedItems field of each hamper.
+	 * inventoryCheckAlgorithm() checks that the missingCategory array is empty and creates a text file, "Order.txt"
+	 * which contains the order information.
+	 */
+	@Test
+	public void testCreateOrderFormCorrectlyCreatesOrderForm(){}
+
+	/*
+	 * InventoryService(Hamper[]) is called with a list of hampers where the first hamper cannot be filled.
+	 * inventoryCheckAlgorithm() calls fillHampers() which then updates tmpUsed field in appropriate FoodItem objects to 
+	 * "true", the canBeFulfilled field in the first Hamper to "false", adds the missing food category to the 
+	 * missingCategory array, and continues to check the second hamper in the hampersToCheck array. 
+	 * inventoryCheckAlgorithm() checks that the missingCategory array is not empty, resets the tmpUsed field of all
+	 * FoodItems in the inventory to "false" and throws the custom InventoryNotAvailableException with a message 
+	 * indicating which food categories are missing.
+	 */ 
+	@Test
+	public void inventoryCheckAlgorithmCorrectlyThrowsException(){}
 
 }
