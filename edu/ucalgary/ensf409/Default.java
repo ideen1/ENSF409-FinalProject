946103d1f3f8a1a96c0abd32c53d6c654c51d136
Ideen B <48538139+ideen1@users.noreply.github.com>
2022-04-04 23:38:53 -0600

    Editing

--- edu/ucalgary/ensf409/Default.java
+++ edu/ucalgary/ensf409/Default.java
@@ -1,7 +1,7 @@
 /** *Default*.java 
  *  Java Class file for ENSF409 Final Project 202 - 
  *  Winter 2022 - Group 5
- *  Copyright © 2022 I.B., T.D., M.G.
+ *  Copyright © 2022 I.B., T.D., M.M.
  *  @author *INSERT NAME*
  *  @version 1.0
  *  @since *INSERT SINCE VERSION*
