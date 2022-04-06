d8b112e9851710bdb298a9116435adafae89f981
Ideen Banijamali <ideen@Ideens-MacBook-Air.local>
2022-04-06 16:09:25 -0600

    Load Inventory Food

--- edu/ucalgary/ensf409/FoodItem.java
+++ edu/ucalgary/ensf409/FoodItem.java
@@ -5,24 +5,24 @@ package edu.ucalgary.ensf409;
 public class FoodItem{
 	private int id;
 	private String name;
-	private int fruitVeggiesContent;
-	private int grainContent;
-	private int proteinContent;
-	private int other;
-	private int calories;
+	private double fruitVeggiesContent;
+	private double grainContent;
+	private double proteinContent;
+	private double other;
+	private double calories;
 	
 	// Getters
 	public int getID() { return this.id; }
 	public String getName() { return this.name; }
-	public int getFruitVeggieContent() { return this.fruitVeggiesContent; }
-	public int getGrainContent() { return this.grainContent; }
-	public int getProteinContent() { return proteinContent; }
-	public int getOther() { return this.other; }
-	public int getCalories() { return this.calories; }
+	public double getFruitVeggieContent() { return this.fruitVeggiesContent; }
+	public double getGrainContent() { return this.grainContent; }
+	public double getProteinContent() { return proteinContent; }
+	public double getOther() { return this.other; }
+	public double getCalories() { return this.calories; }
 	
 	
 	// Constructor
-	public FoodItem(int id, String name, int fvc, int gc, int pc, int other, int cal) {
+	public FoodItem(int id, String name, double fvc, double gc, double pc, double other, double cal) {
 		this.id = id;
 		this.name = name;
 		this.fruitVeggiesContent = fvc;
--- edu/ucalgary/ensf409/Inventory.java
+++ edu/ucalgary/ensf409/Inventory.java
@@ -1,11 +1,37 @@
 package edu.ucalgary.ensf409;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 public class Inventory {
     static private HashMap<Integer,FoodItem> foodList = new HashMap<Integer, FoodItem>();
+    
     static {
         // CODE to load food items from inventory
+        try {
+            DBConnection dbc = new DBConnection();
+            dbc.initializeConnection();
+            ResultSet results = dbc.customQuery("SELECT * FROM AVAILABLE_FOOD");;
+
+            while (results.next()){
+                FoodItem newItem = new FoodItem(
+                    Integer.valueOf(results.getString("ItemID")), 
+                    results.getString("Name"), 
+                    Double.valueOf(results.getString("FVContent")), 
+                    Double.valueOf(results.getString("GrainContent")), 
+                    Double.valueOf(results.getString("ProContent")), 
+                    Double.valueOf(results.getString("Other")), 
+                    Double.valueOf(results.getString("Calories"))
+                    );
+                foodList.put(Integer.valueOf(results.getString("ItemID")) , newItem);
+                
+            } 
+
+        } catch (SQLException e){
+            GUIViewController.genericError("Error retrieving Food Inventory from Database");
+        }
+
     }
     public static HashMap<Integer,FoodItem> getFoodlist(){
         return Inventory.foodList;
