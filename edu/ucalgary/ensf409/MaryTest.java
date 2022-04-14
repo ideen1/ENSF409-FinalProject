35dc47a5f62786411f50e0f6bd28be81d9876f54
Mary Mo <ga.mo@ucalgary.ca>
2022-04-13 22:29:53 -0600

    tests

---
+++ edu/ucalgary/ensf409/MaryTest.java
@@ -0,0 +1,17 @@
+package edu.ucalgary.ensf409;
+
+public class MaryTest {
+    
+    //FoodItem
+    /*
+     * FooodItem() is constructed using test data.
+     * 
+     * 
+     */
+    @Test
+    public void testFoodItemGetters(){
+        
+    }
+        FoodItem cucumber = new FoodItem(1111, "cucumber", 100, 0, 2, 5, 10);
+
+}
